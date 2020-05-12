3c6f340271a7c4aaab98b442663a7f4cf7643cb4
Bálint Áts <atsbalint@gmail.com>
2020-05-12 19:25:04 +0200

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PokemonList from './components/pokemonList';
-import { BrowserRouter as Router, Switch, Route, browserHistory } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavPage from './components/navPage';
+import Home from './components/home';
+import TypeList from './components/typeList';
+import PokemonList from './components/pokemonList';
+import { createBrowserHistory } from 'history'
 
 function App() {
   return (
-    < NavPage />
+    <Router>
+      <Switch>
+        <Route path={"/home"} component={Home} />
+        <Route path={"/pokemonList"} component={PokemonList} />
+        <Route path={"/typelist"} component={TypeList} />
+      </Switch>
+    </Router >
+    // < NavPage />
   );
 }
 
 export default App;
 
-
-
-
-{/*<div className="App"> 
-  <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */}
